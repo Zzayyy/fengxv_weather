897cddb806bec7b5446121ec5543e200f9105fdd
Zzay <1840238422@qq.com>
2025-05-14 20:52:07 +0800

    refactor(fengxv_web): 重命名 Swagger 配置类
    将 SwaggerConfiguration.java 文件重命名为 SwaggerConfig.java，简化了类名。
    删除了未使用的导入语句 Contact 和 License，提高了代码的可读性。

--- fengxv_web/src/main/java/com/zzay/fengxv_weather/config/SwaggerConfig.java
+++ fengxv_web/src/main/java/com/zzay/fengxv_weather/config/SwaggerConfig.java
@@ -1,15 +1,13 @@
 package com.zzay.fengxv_weather.config;
 
 import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.info.License;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-public class SwaggerConfiguration {
+public class SwaggerConfig {
 
     @Bean
     public GroupedOpenApi adminApi() {      // 创建了一个api接口的分组
